d4e0bdab339cb70f3659c5560ceba88388e47618
Cathy Lam <cathy27lam@gmail.com>
2022-06-10 13:28:59 -0400

    audio ref start

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// create a module for our project
+(() => {
+    //create a reference to the main headline (the first h2 element)
+    let mainHeadline = document.querySelector('#main-headline');
+
+    // pause the JS execution here
+    debugger;
+})()
